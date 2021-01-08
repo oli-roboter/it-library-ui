54a9681aadf2bf8556d8623bda3e5f34cf68256d
Oliver Wagner <oliverthomas.wagner@gmail.com>
2021-01-08 13:46:41 +0000

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Title from './components/Title.svelte';
+	export let name;
+
+	let message = `Hello ${name}`
+</script>
+
+<main>
+	<Title greeting {message} />
+</main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/components/Title.svelte
@@ -0,0 +1,5 @@
+<script>
+  let greeting;
+</script>
+
+<h1 class=text-red-100>{greeting}</h1>
